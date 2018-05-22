1c049e5b079674a939ad52bde4dbe2b5e4eb8987
thomasnorris <tom.norris135@gmail.com>
2018-05-22 11:16:20 -0400

    added reg key to open folders in sublime

--- AddItemsToRightClick.bat
+++ AddItemsToRightClick.bat
@@ -1,5 +1,7 @@
 @echo off
 
+:: Add new keys to RemoveItemsFromRightClick.bat!
+
 :::: Add "Open Cmder here" to the shift-right click menu
 set cmderExePath=%CMDER_ROOT%\Cmder.exe
 set cmderBackgroundKey=HKEY_CLASSES_ROOT\Directory\Background\shell\Cmder
@@ -17,17 +19,24 @@ set cmderShellKey=HKEY_CLASSES_ROOT\Directory\shell\Cmder
 @reg add "%cmderShellKey%" /t REG_EXPAND_SZ /v "Icon" /d "%cmderExePath%,0" /f
 @reg add "%cmderShellKey%\command" /t REG_SZ /v "" /d "%cmderExePath% \"%%1\"" /f
 
-:: Add Sublime Text to the list of programs to open files with (for setting as default)
+:::: Add Sublime Text to the list of programs to open files with (for setting as default)
 set sublimePath="%HOME%\sublime_text\sublime_text.exe"
 set sublimeShellKey="HKEY_CLASSES_ROOT\Applications\sublime_text.exe\shell\open\command"
 
 @reg add %sublimeShellKey% /t REG_EXPAND_SZ /v "" /d "%sublimePath% \"%%1\"" /f
 
-:: Add "Edit with Sublime Text" to the right click menu
+:::: Add "Edit with Sublime Text" to the normal right-click menu
 set allFileTypesKey=HKEY_CLASSES_ROOT\*\shell\Edit with Sublime Text
+set foldersKey=HKEY_CLASSES_ROOT\Folder\shell\Open in Sublime Text
 
+:: Sublime all file types key
 @reg add "%allFileTypesKey%" /t REG_SZ /v "" /d "Edit with Sublime Text"   /f
 @reg add "%allFileTypesKey%" /t REG_EXPAND_SZ /v "Icon" /d "%sublimePath%,0" /f
 @reg add "%allFileTypesKey%\command" /t REG_SZ /v "" /d "%sublimePath% \"%%1\"" /f
+
+:: Sublime folders key
+@reg add "%foldersKey%" /t REG_SZ /v "" /d "Open in Sublime Text"   /f
+@reg add "%foldersKey%" /t REG_EXPAND_SZ /v "Icon" /d "%sublimePath%,0" /f
+@reg add "%foldersKey%\command" /t REG_SZ /v "" /d "%sublimePath% \"%%1\"" /f
 
 exit /b 0
--- RemoveItemsFromRightClick.bat
+++ RemoveItemsFromRightClick.bat
@@ -1,9 +1,10 @@
 @echo off
 
-:: Remove Cmder 
+:: Remove Cmder keys
 @reg delete "HKEY_CLASSES_ROOT\Directory\Background\shell\Cmder" /f
 @reg delete "HKEY_CLASSES_ROOT\Directory\shell\Cmder" /f
 
-:: Remove Sublime Text
+:: Remove Sublime Text keys
 @reg delete "HKEY_CLASSES_ROOT\Applications\sublime_text.exe" /f
 @reg delete "HKEY_CLASSES_ROOT\*\shell\Edit with Sublime Text" /f
+@reg delete "HKEY_CLASSES_ROOT\Folder\shell\Open in Sublime Text" /f 

39c7cfaacf9baeb49b40dde925c662492465ff80
thomasnorris <tom.norris135@gmail.com>
2018-06-06 08:29:43 -0400

    key and text renaming with added variables

--- AddRegistryKeys.bat
+++ AddRegistryKeys.bat
@@ -5,11 +5,14 @@
 :: This command will make the item show on the shift right-click menu only
 :: @reg add "%keyNameHere%" /t REG_EXPAND_SZ /v "Extended" /d "" /f
 
+set cmderName=Cmder
+set sublimeName=Sublime Text 3
+
 :::: Add "Open Cmder here" to the shift right-click menu
 set cmderExePath=%CMDER_ROOT%\Cmder.exe
-set cmderBackgroundKey=HKEY_CLASSES_ROOT\Directory\Background\shell\Cmder
-set cmderShellKey=HKEY_CLASSES_ROOT\Directory\shell\Cmder
-set openCmderHereText=Open Cmder here
+set openCmderHereText=Open %cmderName% here
+set cmderBackgroundKey=HKEY_CLASSES_ROOT\Directory\Background\shell\%cmderName%
+set cmderShellKey=HKEY_CLASSES_ROOT\Directory\shell\%cmderName%
 
 @reg add "%cmderBackgroundKey%" /t REG_EXPAND_SZ /v "Extended" /d "" /f 
 @reg add "%cmderBackgroundKey%" /t REG_SZ /v "" /d "%openCmderHereText%" /f
@@ -29,7 +32,7 @@ set sublimeShellKey="HKEY_CLASSES_ROOT\Applications\sublime_text.exe\shell\open\
 @reg add %sublimeShellKey% /t REG_EXPAND_SZ /v "" /d "%sublimeExePath% \"%%1\"" /f
 
 :::: Add "Edit with Sublime Text" to the shift right-click menu for files and folders
-set editWithSublimeText=Edit with Sublime Text
+set editWithSublimeText=Edit with %sublimeName%
 set allFileTypesKey=HKEY_CLASSES_ROOT\*\shell\%editWithSublimeText%
 
 @reg add "%allFileTypesKey%" /t REG_EXPAND_SZ /v "Extended" /d "" /f 
@@ -38,8 +41,8 @@ set allFileTypesKey=HKEY_CLASSES_ROOT\*\shell\%editWithSublimeText%
 @reg add "%allFileTypesKey%\command" /t REG_SZ /v "" /d "%sublimeExePath% \"%%1\"" /f
 
 ::: Add "Create new file here" to the shift right-click menu
-set sublimeBackgroundKey=HKEY_CLASSES_ROOT\Directory\Background\shell\Sublime Text
 set createNewFileHereText=Create new file here
+set sublimeBackgroundKey=HKEY_CLASSES_ROOT\Directory\Background\shell\%sublimeName%
 
 @reg add "%sublimeBackgroundKey%" /t REG_EXPAND_SZ /v "Extended" /d "" /f
 @reg add "%sublimeBackgroundKey%" /t REG_SZ /v "" /d "%createNewFileHereText%" /f
@@ -49,13 +52,13 @@ set createNewFileHereText=Create new file here
 
 :::: Add cascading context menu for Sublime Text 3 when clicking on folders
 set openInSublimeText=Open folder
-set sublimeCascadingKey=HKEY_CLASSES_ROOT\Directory\shell\Sublime Text
+set sublimeCascadingKey=HKEY_CLASSES_ROOT\Directory\shell\%sublimeName%
 set openInSublimeCommandKey=HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\%openInSublimeText%
 set createNewFileCommandKey=HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\%createNewFileHereText%
 
 @reg add "%sublimeCascadingKey%" /t REG_EXPAND_SZ /v "Extended" /d "" /f
 @reg add "%sublimeCascadingKey%" /t REG_EXPAND_SZ /v "Icon" /d "%sublimeExePath%,0" /f
-@reg add "%sublimeCascadingKey%" /t REG_EXPAND_SZ /v "MUIVerb" /d "Sublime Text 3" /f
+@reg add "%sublimeCascadingKey%" /t REG_EXPAND_SZ /v "MUIVerb" /d "%sublimeName%" /f
 @reg add "%sublimeCascadingKey%" /t REG_EXPAND_SZ /v "SubCommands" /d "%openInSublimeText%;%createNewFileHereText%" /f
 
 @reg add "%openInSublimeCommandKey%" /t REG_EXPAND_SZ /v "Icon" /d "%sublimeExePath%,0" /f

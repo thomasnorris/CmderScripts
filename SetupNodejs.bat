1f8a746afabf5fcee5db0e93dda7498b66300bdb
thomasnorris <tom.norris135@gmail.com>
2018-06-18 14:20:56 -0400

    removed setting up node.js from separate file; edited help

--- SetupNodejs.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set "PATH=%HOME%\nodejs;%PATH%"
-echo node and npm have been added to PATH
-
-:: sets global npm packages to be installed in this location
-npm config set prefix "%HOME%\.npm"
-exit /b 0
--- ShowHelp.bat
+++ ShowHelp.bat
@@ -6,8 +6,7 @@ echo. && echo 3. The user startup file is "user-profile.cmd". Startup commands g
 echo. && echo 4. Personal files are found in the "Cmder\personal" folder.
 echo. && echo 5. Personal files are stored on Dropbox. They can be downloaded by running "dl".
 echo. && echo 6. Personal files can be uploaded to Dropbox with "upl". Do this after making any changes (and versioning).
-echo. && echo 7. Nodejs and NPM are already installed but not injected to the path until "n" has been run.
-echo. && echo 9. Run "runbat" in an elevated cmd after install to run useful batch scripts and create shortcuts.
-echo. && echo 10. The changelog is located at "Cmder\personal\changelog.md". Edit this with changes!
-echo. && echo 10. Show this any time with "h".
+echo. && echo 7. Run "runbat" in an elevated cmd after install to run useful batch scripts and create shortcuts.
+echo. && echo 8. The changelog is located at "Cmder\personal\changelog.md". Edit this with changes!
+echo. && echo 9. Show this any time with "h".
 exit /b 0

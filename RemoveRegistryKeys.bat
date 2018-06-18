12c4dd2812f1c4f50273733b627d39b83b130723
thomasnorris <tom.norris135@gmail.com>
2018-06-18 12:54:23 -0400

    switched registry keys for vscode

--- RemoveRegistryKeys.bat
+++ RemoveRegistryKeys.bat
@@ -1,18 +1,19 @@
 @echo off
 
 set cmderName=Cmder
-set sublimeName=Sublime Text 3
-set createNewFileHereText=Create new file here
+set vsCodeName=VSCode
+set createNewFileVsCode=Create new file here
 set openInSublimeText=Open folder
+set openInVsCode=Open folder
 
 :: Remove Cmder keys
 @reg delete "HKEY_CLASSES_ROOT\Directory\Background\shell\%cmderName%" /f
 @reg delete "HKEY_CLASSES_ROOT\Directory\shell\%cmderName%" /f
 
-:: Remove Sublime Text keys
-@reg delete "HKEY_CLASSES_ROOT\Applications\sublime_text.exe" /f
-@reg delete "HKEY_CLASSES_ROOT\*\shell\Edit with %sublimeName%" /f
-@reg delete "HKEY_CLASSES_ROOT\Directory\Background\shell\%sublimeName%" /f
-@reg delete "HKEY_CLASSES_ROOT\Directory\shell\%sublimeName%" /f
-@reg delete "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\%openInSublimeText%" /f
-@reg delete "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\%createNewFileHereText%" /f
+::VSCode Keys
+@reg delete "HKEY_CLASSES_ROOT\Applications\VSCodePortable.exe" /f
+@reg delete "HKEY_CLASSES_ROOT\*\shell\Edit with %vsCodeName%" /f
+@reg delete "HKEY_CLASSES_ROOT\Directory\Background\shell\%vsCodeName%" /f
+@reg delete "HKEY_CLASSES_ROOT\Directory\shell\%vsCodeName%" /f
+@reg delete "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\%openInVsCode%" /f
+@reg delete "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\%createNewFileVsCode%" /f

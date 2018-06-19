127abe0cd7b68b295fd2c18417103e291b971c5c
thomasnorris <tom.norris135@gmail.com>
2018-06-19 10:06:33 -0400

    removed cascading context menu and new file creation

--- AddRegistryKeys.bat
+++ AddRegistryKeys.bat
@@ -27,7 +27,7 @@ reg add "%cmderShellKey%\command" /t REG_SZ /v "" /d "%cmderExePath% \"%%1\"" /f
 
 :::: Add VSCode to the list of programs to open files with (for setting as default)
 set vscodeExePath="%HOME%\vscode\VSCodePortable.exe"
-set vscodeShellKey="HKEY_CLASSES_ROOT\Applications\VSCodePortable.exe\shell\open\command"
+set vscodeShellKey=HKEY_CLASSES_ROOT\Applications\VSCodePortable.exe\shell\open\command
 
 reg add %vscodeShellKey% /t REG_EXPAND_SZ /v "" /d "%vscodeExePath% \"%%1\"" /f
 
@@ -35,38 +35,18 @@ reg add %vscodeShellKey% /t REG_EXPAND_SZ /v "" /d "%vscodeExePath% \"%%1\"" /f
 set editWithVsCodeText=Edit with %vsCodeName%
 set allFileTypesVsCodeKey=HKEY_CLASSES_ROOT\*\shell\%editWithVsCodeText%
 
-reg add "%allFileTypesVsCodeKey%" /t REG_EXPAND_SZ /v "Extended" /d "" /f 
+reg add "%allFileTypesVsCodeKey%" /t REG_EXPAND_SZ /v "Extended" /d "" /f
 reg add "%allFileTypesVsCodeKey%" /t REG_SZ /v "" /d "%editWithVsCodeText%" /f
 reg add "%allFileTypesVsCodeKey%" /t REG_EXPAND_SZ /v "Icon" /d "%vscodeExePath%,0" /f
 reg add "%allFileTypesVsCodeKey%\command" /t REG_SZ /v "" /d "%vscodeExePath% \"%%1\"" /f
 
-::: Add "Create new file here" to the shift right-click menu
-set createNewFileVsCode=Create new file here
-set vsCodeBackgroundKey=HKEY_CLASSES_ROOT\Directory\Background\shell\%vsCodeName%
+:::: Add "Open in VSCode" to the shift right-click menu for folders
+set openWithVsCodeText=Open with %vsCodeName%
+set vsCodeFoldersKey=HKEY_CLASSES_ROOT\Directory\shell\%openWithVsCodeText%
 
-reg add "%vsCodeBackgroundKey%" /t REG_EXPAND_SZ /v "Extended" /d "" /f
-reg add "%vsCodeBackgroundKey%" /t REG_SZ /v "" /d "%createNewFileVsCode%" /f
-reg add "%vsCodeBackgroundKey%" /t REG_EXPAND_SZ /v "Icon" /d "%vscodeExePath%,0" /f
-reg add "%vsCodeBackgroundKey%\command" /t REG_EXPAND_SZ /v "Extended" /d "" /f
-reg add "%vsCodeBackgroundKey%\command" /t REG_SZ /v "" /d "%vscodeExePath% \"untitled\"" /f
-
-:::: Add cascading context menu for VSCode when clicking on folders
-set openInVsCode=Open folder
-set vsCodeCascadingKey=HKEY_CLASSES_ROOT\Directory\shell\%vsCodeName%
-set openInVsCodeCommandKey=HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\%openInVsCode%
-set createNewFileCommandKey=HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\%createNewFileVsCode%
-
-reg add "%vsCodeCascadingKey%" /t REG_EXPAND_SZ /v "Extended" /d "" /f
-reg add "%vsCodeCascadingKey%" /t REG_EXPAND_SZ /v "Icon" /d "%vscodeExePath%,0" /f
-reg add "%vsCodeCascadingKey%" /t REG_EXPAND_SZ /v "MUIVerb" /d "%vsCodeName%" /f
-reg add "%vsCodeCascadingKey%" /t REG_EXPAND_SZ /v "SubCommands" /d "%openInVsCode%;%createNewFileVsCode%" /f
-
-reg add "%openInVsCodeCommandKey%" /t REG_EXPAND_SZ /v "Icon" /d "%vscodeExePath%,0" /f
-reg add "%openInVsCodeCommandKey%" /t REG_SZ /v "" /d "%openInVsCode%" /f
-reg add "%openInVsCodeCommandKey%\command" /t REG_SZ /v "" /d "%vscodeExePath% \"%%1\"" /f
-
-reg add "%createNewFileCommandKey%" /t REG_EXPAND_SZ /v "Icon" /d "%vscodeExePath%,0" /f
-reg add "%createNewFileCommandKey%" /t REG_SZ /v "" /d "%createNewFileVsCode%" /f
-reg add "%createNewFileCommandKey%\command" /t REG_SZ /v "" /d "%vscodeExePath% \"%%1\untitled\"" /f
+reg add "%vsCodeFoldersKey%" /t REG_EXPAND_SZ /v "Extended" /d "" /f
+reg add "%vsCodeFoldersKey%" /t REG_SZ /v "" /d "%openWithVsCodeText%"   /f
+reg add "%vsCodeFoldersKey%" /t REG_EXPAND_SZ /v "Icon" /d "%vscodeExePath%,0" /f
+reg add "%vsCodeFoldersKey%\command" /t REG_SZ /v "" /d "%vscodeExePath% \"%%1\"" /f
 
 exit /b 0

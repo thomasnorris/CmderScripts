d4b7aed8a1b3d6fd74da0ed87f489d5a640fbe52
thomasnorris <tom.norris135@gmail.com>
2018-06-21 12:13:58 -0400

    edited help

--- ShowHelp.bat
+++ ShowHelp.bat
@@ -6,7 +6,6 @@ echo. && echo 3. The user startup file is "user-profile.cmd". Startup commands g
 echo. && echo 4. Personal files are found in the "Cmder\personal" folder.
 echo. && echo 5. Personal files are stored on Dropbox. They can be downloaded by running "dl".
 echo. && echo 6. Personal files can be uploaded to Dropbox with "upl". Do this after making any changes (and versioning).
-echo. && echo 7. Run "runbat" in an elevated cmd after install to run useful batch scripts and create shortcuts.
-echo. && echo 8. The changelog is located at "Cmder\personal\changelog.md". Edit this with changes!
-echo. && echo 9. Show this any time with "h".
+echo. && echo 7. The changelog is located at "Cmder\personal\changelog.md". Edit this with changes!
+echo. && echo 8. Show this any time with "h".
 exit /b 0

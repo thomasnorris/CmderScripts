05fce27bb2d60c82c991d904669ef77966dd7747
thomasnorris <tom.norris135@gmail.com>
2018-07-09 09:45:17 -0400

    ignore temp and logs folders for VSCode when uploading

--- UploadConfigs.bat
+++ UploadConfigs.bat
@@ -20,7 +20,8 @@ cd /d %uploadDir%
 :: Use -xr! to ignore files and folders
 echo Adding files to "%configArchiveName%", please wait...
 7za a %configArchiveName% "%CMDER_ROOT%\config" > nul
-7za a %configArchiveName% "%HOME%" -xr!"personal\vscode\Data\code\Cache*\" > nul
+set vsCodeIgnoreRoot=personal\vscode\Data
+7za a %configArchiveName% "%HOME%" -xr!"%vsCodeIgnoreRoot%\code\Cache*\" -xr!"%vsCodeIgnoreRoot%\code\logs\" -xr!"%vsCodeIgnoreRoot%\Temp\" > nul
 7za a %configArchiveName% "%CMDER_ROOT%\vendor\conemu-maximus5\ConEmu.xml" > nul
 
 :: Create a separate archive for the "Setup" scripts

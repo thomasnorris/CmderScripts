6c00dc197ecc50cabe343d128df11201e4f3f341
Thomas Norris <tom.norris135@gmail.com>
2020-02-06 15:18:58 -0500

    added script to store latest dropbox revision

---
+++ StoreDropboxRevision.bat
@@ -0,0 +1,26 @@
+@echo off
+Setlocal EnableDelayedExpansion
+
+:: Assuming this file is in the root of the %HOME% directory
+:: Must match in user-profile.cmd!
+set configRevisionFile=config_revision.txt
+
+:: These must match the same variables in UploadConfigs.bat
+set uploadFolderInDropbox=Cmder Files
+set configArchiveName=Config.7z
+
+set revs=
+for /f %%a in ('dbxcli revs "/%uploadFolderInDropbox%/%configArchiveName%"') do (
+    set revs=!revs! %%a
+)
+
+for /f "tokens=1" %%a in ("%revs%") do (
+    set rev=%%a
+)
+
+Setlocal DisableDelayedExpansion
+
+echo %rev% > "%HOME%\%configRevisionFile%"
+echo Revision %rev% stored.
+
+exit /b 0
